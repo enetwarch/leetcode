88fda17ea95b8fa6b7d2d7c9a3550299240c4d43
enetwarch <enetwarch@protonmail.com>
2025-07-25 17:02:01 +0800

    refactor: medium #125

--- 0101-0200/0125-valid-palindrome.cpp
+++ 0101-0200/0125-valid-palindrome.cpp
@@ -1,9 +1,15 @@
 #include <cctype>
 #include <string>
 
+// https://leetcode.com/problems/valid-palindrome/
 class Solution {
    public:
     bool isPalindrome(std::string s) {
+        // Space complexity is O(1) as it does not scale.
+        // Time complexity is O(n) for both looped algos.
+
+        // Input cleaning algorithm for easier calculation.
+        // Directly modifies the input string to save memory.
         int i = 0;
         for (auto c : s) {
             if (!std::isalnum(c)) continue;
@@ -11,6 +17,8 @@ class Solution {
         }
         s.erase(i);
 
+        // Two pointer strategy where we move both pointers inwards.
+        // More efficient than reversing a string and comparing.
         int l = 0;
         int r = s.size() - 1;
         while (l < r) {
