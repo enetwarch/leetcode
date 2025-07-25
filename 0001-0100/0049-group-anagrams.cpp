13556c7d4777dd08741b6ee49a08b8a2230a1ac0
enetwarch <enetwarch@protonmail.com>
2025-07-25 16:34:03 +0800

    refactor: medium #49

--- 0001-0100/0049-group-anagrams.cpp
+++ 0001-0100/0049-group-anagrams.cpp
@@ -3,13 +3,18 @@
 #include <unordered_map>
 #include <vector>
 
+// https://leetcode.com/problems/group-anagrams/
 class Solution {
    public:
     std::vector<std::vector<std::string>> groupAnagrams(
         std::vector<std::string>& strs) {
-        std::vector<std::vector<std::string>> soln;
+        // For complexity, n will be strings, k will be their length.
+        // Space complexity is O(nk) for both the map and output.
         std::unordered_map<std::string, std::vector<std::string>> map;
 
+        // Sorts each string to serve as a key for the hashmap.
+        // Anagrams will always have the same sorted string.
+        // Time complexity will be O(nk log(k)); simple loop with sorting.
         for (auto str : strs) {
             std::string sorted = str;
             std::sort(sorted.begin(), sorted.end());
@@ -17,6 +22,8 @@ class Solution {
             map[sorted].push_back(str);
         }
 
+        // Output parsing part from map to vector.
+        std::vector<std::vector<std::string>> soln;
         for (auto entry : map) {
             soln.push_back(entry.second);
         }
