804004a2a1a06b1a1d1e2815461a93a3fbf9148b
enetwarch <enetwarch@protonmail.com>
2025-07-25 17:05:55 +0800

    refactor: easy #242

--- 0201-0300/0242-valid-anagram.cpp
+++ 0201-0300/0242-valid-anagram.cpp
@@ -1,18 +1,24 @@
 #include <string>
 #include <unordered_map>
 
+// https://leetcode.com/problems/valid-anagram/
 class Solution {
    public:
     bool isAnagram(std::string s, std::string t) {
+        // Early guard clause to potentially save on performance.
+        // Space complexity is O(n) where n is size of either strings.
         if (s.size() != t.size()) return false;
         std::unordered_map<char, int> map;
 
         // All map values should equalize to 0 if it is an anagram.
+        // Both this and the checking algo are O(n) in time complexity.
         for (int i = 0; i < s.size(); i++) {
             map[s[i]]++;
             map[t[i]]--;
         }
 
+        // Checks if a value in the map is unequalized.
+        // It would mean there is a value that isn't equal.
         for (auto c : map) {
             if (c.second != 0) return false;
         }
