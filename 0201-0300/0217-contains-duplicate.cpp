d60558877bae2844aaf841fd1e8af668f03328b0
enetwarch <enetwarch@protonmail.com>
2025-07-25 17:08:00 +0800

    refactor: easy #217

--- 0201-0300/0217-contains-duplicate.cpp
+++ 0201-0300/0217-contains-duplicate.cpp
@@ -1,14 +1,15 @@
 #include <unordered_set>
 #include <vector>
 
+// https://leetcode.com/problems/contains-duplicate/
 class Solution {
    public:
     bool containsDuplicate(std::vector<int>& nums) {
-        // Best case scenario, hashsets have O(1) look up and insertion time.
-        // Allocation is like this to try to avoid collisions and rehashing.
-        // NOLINTNEXTLINE(bugprone-narrowing-conversions)
-        std::unordered_set<int> set(nums.size() * 1.5);
+        // Best case scenario, hashsets have O(1) on most operations.
+        // Space complexity is O(n) worst case scenario.
+        std::unordered_set<int> set;
 
+        // Time complexity is O(n) if there is no duplicate.
         for (int num : nums) {
             if (set.contains(num)) return true;
             set.insert(num);
