6027a1c3f1ba1b61a81bd430c1ae27c965ccdc42
enetwarch <enetwarch@protonmail.com>
2025-08-23 20:17:16 +0800

    style: applied formatter

--- 0201-0300/0206-reverse-linked-list.cpp
+++ 0201-0300/0206-reverse-linked-list.cpp
@@ -2,16 +2,15 @@
 
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
-
 
 // https://leetcode.com/problems/reverse-linked-list/
 class Solution {
-public:
+   public:
     ListNode* reverseList(ListNode* head) {
         ListNode* previous_node = nullptr;
         ListNode* current_node = head;
