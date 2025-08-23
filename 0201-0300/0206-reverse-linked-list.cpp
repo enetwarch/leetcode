2a2f315bb8fe549e3be9fa3e7132ba0550d12d8b
enetwarch <enetwarch@protonmail.com>
2025-08-23 22:01:08 +0800

    refactor: easy #206 added comments

--- 0201-0300/0206-reverse-linked-list.cpp
+++ 0201-0300/0206-reverse-linked-list.cpp
@@ -1,3 +1,4 @@
+/*
 struct ListNode {
     int val;
     ListNode* next;
@@ -5,14 +6,21 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
+*/
 
 // https://leetcode.com/problems/reverse-linked-list/
 class Solution {
    public:
     ListNode* reverseList(ListNode* head) {
+        // Keep track of current and previous node.
+        // Space complexity is O(1); does not scale.
         ListNode* previous_node = nullptr;
         ListNode* current_node = head;
 
+        // Next node is first stored to allow the current node to advance.
+        // Simply turns the pointer of the current node to the previous.
+        // After that, the current node will advance to the stored next node.
+        // Time complexity is O(n); linearly iterates through each node.
         while (current_node != nullptr) {
             ListNode* next_node = current_node->next;
             current_node->next = previous_node;
