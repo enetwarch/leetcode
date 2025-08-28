f3263eb8083f47f9644e9f346f65543ca10af1c3
enetwarch <enetwarch@protonmail.com>
2025-08-28 15:33:36 +0800

    feat: easy #100

---
+++ 0001-0100/0100-same-tree.cpp
@@ -0,0 +1,56 @@
+#include <queue>
+
+/*
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+right(right) {}
+};
+*/
+
+// https://leetcode.com/problems/same-tree/
+class Solution {
+   public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // Queues are used to enable BFS algorithm.
+        // Space complexity is O(n*m); queues stores both tree nodes.
+        if (p == nullptr && q != nullptr) return false;
+        std::queue<TreeNode*> pqueue;
+        std::queue<TreeNode*> qqueue;
+        pqueue.push(p);
+        qqueue.push(q);
+
+        // This loop performs the BFS algorithm on both trees.
+        // Time complexity is O(n); Iterates on both tree nodes at once.
+        while ((!pqueue.empty())) {
+            TreeNode* ppointer = pqueue.front();
+            TreeNode* qpointer = qqueue.front();
+            pqueue.pop();
+            qqueue.pop();
+
+            // This statement is the guard clause to check inequality.
+            // If one of the nodes are null and the other isn't, return false.
+            // If the values are not equal, return false.
+            // Also checks left and right pointers in advance if they are null.
+            if ((ppointer == nullptr ^ qpointer == nullptr) ||
+                (ppointer != nullptr &&
+                 ((ppointer->val != qpointer->val) ||
+                  (ppointer->left == nullptr && qpointer->left != nullptr) ||
+                  (ppointer->right == nullptr && qpointer->right != nullptr))))
+                return false;
+
+            if (ppointer != nullptr) {
+                pqueue.push(ppointer->left);
+                pqueue.push(ppointer->right);
+                qqueue.push(qpointer->left);
+                qqueue.push(qpointer->right);
+            }
+        }
+
+        return true;
+    }
+};
